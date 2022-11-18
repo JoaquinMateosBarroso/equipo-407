9b5506c2d3b2075459ea805d0f3eb4384ad197b1
JoaquinMateosBarroso <jmateosbarroso@gmail.com>
2022-11-18 18:54:04 +0100

    Constructor por defecto Usuario

--- marketplace/src/usuario/usuario.h
+++ marketplace/src/usuario/usuario.h
@@ -7,12 +7,17 @@
 
 class Usuario{
     private:
-        unsigned usuario;
-        std::string nombre;
-        std::string apellidos;
-        std::string dni;
-        std::string correo;
-        std::string rol;
+        unsigned usuario_;
+        std::string nombre_;
+        std::string apellidos_;
+        std::string dni_;
+        std::string correo_;
+        std::string rol_;
+    public:
+        Usuario(unsigned usuario, std::string nombre, std::string apellidos, 
+        std::string dni, std::string correo, std::string rol) : 
+        usuario_(usuario), nombre_(nombre), apellidos_(apellidos),
+        dni_(dni), correo_(correo), rol_(rol) {}
 };
 
 #endif
